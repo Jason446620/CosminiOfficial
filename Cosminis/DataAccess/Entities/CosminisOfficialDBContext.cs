0f805272a81c0a6aca39ef571ee6ba4ebeacdbb0
unknown <lorelailyons@protonmail.com>
2024-08-02 15:12:48 -0400

    Fixed an error message and connected the database.

---
+++ Cosminis/DataAccess/Entities/CosminisOfficialDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataAccess.Entities
+{
+    public partial class CosminisOfficialDBContext : DbContext
+    {
+        public CosminisOfficialDBContext()
+        {
+        }
+
+        public CosminisOfficialDBContext(DbContextOptions<CosminisOfficialDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
